8853276c44f9142a6c8d567fbaf87abc589a19cc
Anna Sak <ann194341@gmail.com>
2024-12-28 12:56:25 +0200

    3. Конструкції if замінено на switch

--- main/java/com/gildedrose/GildedRose.java
+++ main/java/com/gildedrose/GildedRose.java
@@ -9,41 +9,36 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals("Aged Brie")
-                    || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality < 50) {
-                            Item.increasesQualityByOne(item);
-
-                            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                                if (item.sellIn < 11) {
-                                    Item.increasesQualityByOne(item);
-                                }
-
-                                if (item.sellIn < 6) {
-                                    Item.increasesQualityByOne(item);
-                                }
-                            }
-                        }
-                    } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                Item.decreaseQualityByOne(item);
-            }
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.name.equals("Aged Brie")) {
-                if (item.sellIn < 0) {
+            switch (item.name) {
+                case "Aged Brie":
                     Item.increasesQualityByOne(item);
-                }
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
-            } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                if (item.sellIn < 0) {
+                    item.sellIn = item.sellIn - 1;
+                    if (item.sellIn < 0) {
+                        Item.increasesQualityByOne(item);
+                    }
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    Item.increasesQualityByOne(item);
+                    if (item.sellIn < 11) {
+                        Item.increasesQualityByOne(item);
+                    }
+                    if (item.sellIn < 6) {
+                        Item.increasesQualityByOne(item);
+                    }
+                    item.sellIn = item.sellIn - 1;
+                    if (item.sellIn < 0) {
+                        item.quality = 0;
+                    }
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                default:
                     Item.decreaseQualityByOne(item);
-                }
+                    item.sellIn = item.sellIn - 1;
+                    if (item.sellIn < 0) {
+                        Item.decreaseQualityByOne(item);
+                    }
+                    break;
             }
         }
     }
